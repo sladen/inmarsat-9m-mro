363b58356a4142329c3cc956a07c784d9fc73373
Paul Sladen <9m-mro@paul.sladen.org>
2014-07-22 11:56:51 +0100

    Remove carried over duplication

--- aaza-dana/aaza-dana.sh
+++ aaza-dana/aaza-dana.sh
@@ -3,6 +3,7 @@
 echo 'c5c93ca2092aea61c2bf247f76ff95f7d692f91e  MH370.gdb' | sha1sum -c - && \
 gpsbabel -t -i gdb -f MH370.gdb \
   -x nuketypes,waypoints,routes \
+  -x track,merge \
   -x track,start=201403071600,stop=201403080201 \
   -x track,title="Azaa Dana GPS extract 2014-03-07 16:00 to 2014-03-08 02:01" \
   -o gpx -F - \
